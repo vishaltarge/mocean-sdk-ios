98a700eb516147e80081b6f206f0cc75ff67cf02
Constantine Mureev <mureev@gmail.com>
2011-09-20 10:45:29 +0400

    add "<!-- Error: 1 ->" server response handler

--- Sources/Classes/Ad/AdDescriptor/AdDescriptor.m
+++ Sources/Classes/Ad/AdDescriptor/AdDescriptor.m
@@ -31,11 +31,11 @@
             
             if ([adDescriptor.serverReponseString rangeOfString:@"<!-- invalid params -->"].location != NSNotFound) {
                 adDescriptor.adContentType = AdContentTypeInvalidParams;
-            }
-            else if ([AdDescriptorHelper isVideoContent:adDescriptor.serverReponseString]) {
+            } else if ([adDescriptor.serverReponseString rangeOfString:@"<!-- Error: 1 ->"].location != NSNotFound) {
+                adDescriptor.adContentType = AdContentTypeInvalidParams;
+            } else if ([AdDescriptorHelper isVideoContent:adDescriptor.serverReponseString]) {
                 adDescriptor.adContentType = AdContentTypeMojivaVideo;
-            }
-            else if ([AdDescriptorHelper isExternalCampaign:adDescriptor.serverReponseString]) {
+            } else if ([AdDescriptorHelper isExternalCampaign:adDescriptor.serverReponseString]) {
                 ServerXMLResponseParser* _xmlResponseParser = [[ServerXMLResponseParser alloc] init];
                 [_xmlResponseParser startParseSynchronous:adDescriptor.serverReponseString];
                 
@@ -44,14 +44,12 @@
                     adDescriptor.adId = _xmlResponseParser.adId;
                     adDescriptor.campaignId = _xmlResponseParser.campaignId;
                     adDescriptor.trackUrl = _xmlResponseParser.trackUrl;
-                }
-                else if (_xmlResponseParser.adContentType == AdContentTypeGreystripe) {
+                } else if (_xmlResponseParser.adContentType == AdContentTypeGreystripe) {
                     adDescriptor.adContentType = AdContentTypeGreystripe;
                     adDescriptor.appId = _xmlResponseParser.appId;
                     adDescriptor.campaignId = _xmlResponseParser.campaignId;
                     adDescriptor.trackUrl = _xmlResponseParser.trackUrl;
-                }
-                else if (_xmlResponseParser.adContentType == AdContentTypeMillennial) {
+                } else if (_xmlResponseParser.adContentType == AdContentTypeMillennial) {
                     adDescriptor.adContentType = AdContentTypeMillennial;
                     adDescriptor.appId = _xmlResponseParser.appId;
                     adDescriptor.campaignId = _xmlResponseParser.campaignId;
@@ -60,14 +58,12 @@
                     adDescriptor.latitude = _xmlResponseParser.latitude;
                     adDescriptor.longitude = _xmlResponseParser.longitude;
                     adDescriptor.zip = _xmlResponseParser.zip;
-                }
-                else if (_xmlResponseParser.adContentType == AdContentTypeiVdopia) {
+                } else if (_xmlResponseParser.adContentType == AdContentTypeiVdopia) {
                     adDescriptor.adContentType = AdContentTypeiVdopia;
                     adDescriptor.appId = _xmlResponseParser.appId;
                     adDescriptor.campaignId = _xmlResponseParser.campaignId;
                     adDescriptor.trackUrl = _xmlResponseParser.trackUrl;
-                }
-                else if (_xmlResponseParser.adContentType == AdContentTypeAdMob) {
+                } else if (_xmlResponseParser.adContentType == AdContentTypeAdMob) {
                     adDescriptor.adContentType = AdContentTypeAdMob;
                     adDescriptor.appId = _xmlResponseParser.appId;
                     adDescriptor.latitude = _xmlResponseParser.latitude;
@@ -75,42 +71,36 @@
                     adDescriptor.zip = _xmlResponseParser.zip;
                     adDescriptor.campaignId = _xmlResponseParser.campaignId;
                     adDescriptor.trackUrl = _xmlResponseParser.trackUrl;
-                }
-                else if (_xmlResponseParser.adContentType == AdContentTypeRhythm) {
+                } else if (_xmlResponseParser.adContentType == AdContentTypeRhythm) {
                      adDescriptor.adContentType = AdContentTypeRhythm;
                      adDescriptor.appId = _xmlResponseParser.appId;
                      adDescriptor.campaignId = _xmlResponseParser.campaignId;
                      adDescriptor.trackUrl = _xmlResponseParser.trackUrl;
-                }
-                else if (_xmlResponseParser.adContentType == AdContentTypeSAS) {
+                } else if (_xmlResponseParser.adContentType == AdContentTypeSAS) {
                     adDescriptor.adContentType = AdContentTypeSAS;
                     adDescriptor.appId = _xmlResponseParser.appId;
                     adDescriptor.adId = _xmlResponseParser.adId;
                     adDescriptor.adType = _xmlResponseParser.adType;
                     adDescriptor.campaignId = _xmlResponseParser.campaignId;
                     adDescriptor.trackUrl = _xmlResponseParser.trackUrl;
-                }
-                else {
+                } else {
                     adDescriptor.adContentType = AdContentTypeUndefined;
                 }
                 
                 [_xmlResponseParser release];
-            }
-            else {
+            } else {
                 NSString* clearHtml = [AdDescriptorHelper stringByStrippingHTMLcomments:adDescriptor.serverReponseString];
                 if ([clearHtml length] > 0) {
                     adDescriptor.adContentType = AdContentTypeDefaultHtml;
                     adDescriptor.serverReponseString = [AdDescriptorHelper wrapHTML:clearHtml frameSize:frameSize aligmentCenter:aligmentCenter];
                     adDescriptor.serverReponse = [adDescriptor.serverReponseString dataUsingEncoding:NSUTF8StringEncoding];
-                }
-                else {
+                } else {
                     adDescriptor.adContentType = AdContentTypeUndefined;
                 }
                 
             }
         }
-	}
-	else {
+	} else {
 		adDescriptor.adContentType = AdContentTypeUndefined;
 	}
 	
