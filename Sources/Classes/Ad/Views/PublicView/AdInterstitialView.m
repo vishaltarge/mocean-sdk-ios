eafa6fd345436a37e5497834b94ca9bd90922399
Constantine Mureev <mureev@gmail.com>
2011-12-01 11:04:23 +0400

    small fixes for interstitial

--- Sources/Classes/Ad/Views/PublicView/AdInterstitialView.m
+++ Sources/Classes/Ad/Views/PublicView/AdInterstitialView.m
@@ -77,42 +77,88 @@
 - (void)dislpayAd:(NSNotification*)notification {
 	NSDictionary *info = [notification object];
 	AdView* adView = [info objectForKey:@"adView"];
+	UIView* subView = [info objectForKey:@"subView"];
+    NSString* contentWidth = [info objectForKey:@"contentWidth"];
+    NSString* contentHeight = [info objectForKey:@"contentHeight"];
 	
 	if (adView == self) {
-        //[super dislpayAd:notification];
-		
-		// Close button code
-		if (!self.closeButton && !self.adModel.useCustomClose) {
-            [self prepareResources];
-			
-			self.closeButton.frame = CGRectMake(self.frame.size.width - self.closeButton.frame.size.width - 11, 11, self.closeButton.frame.size.width, self.closeButton.frame.size.height);
-            self.closeButton.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin;
+        AdModel* model = [self adModel];
+        UIView* currentAdView = model.currentAdView;
+        if (subView != currentAdView) {            
+            model.snapshot = currentAdView;
+            [self adModel].snapshotRAWData = nil;
             
-            self.closeButton.hidden = YES;
+            model.currentAdView = subView;
+            subView.hidden = NO;
             
-            [self addSubview:self.closeButton];
-            if (((AdModel*)_adModel).showCloseButtonTime > 0 && !((AdModel*)_adModel).isDisplayed) {
-                [NSTimer scheduledTimerWithTimeInterval:((AdModel*)_adModel).showCloseButtonTime
-                                                 target:self 
-                                               selector:@selector(showCloseButton)
-                                               userInfo:nil 
-                                                repeats:NO];
+            // update content size if possible
+            if (contentHeight && contentWidth) {
+                model.contentSize = CGSizeMake([contentWidth floatValue], [contentHeight floatValue]);
+            } else {
+                model.contentSize = CGSizeZero;
             }
-            else {
-                [self showCloseButton];
+            
+            // switch animation
+            if (model.animateMode && currentAdView && subView) {
+                CGRect prevAdFrame = subView.frame;
+                CGRect startAdFrame = CGRectMake(prevAdFrame.origin.x-prevAdFrame.size.width, prevAdFrame.origin.y, prevAdFrame.size.width, prevAdFrame.size.height);
+                subView.frame = startAdFrame;
+                
+                [UIView animateWithDuration:0.2 animations:^{
+                    subView.frame = prevAdFrame;
+                    CGRect newFrameForOldImage = CGRectMake(prevAdFrame.origin.x+prevAdFrame.size.width, prevAdFrame.origin.y, prevAdFrame.size.width, prevAdFrame.size.height);
+                    currentAdView.frame = newFrameForOldImage;
+                } completion:^(BOOL finished) {
+                    UIView* oldView = model.snapshot;
+                    
+                    if (oldView && oldView.superview) {
+                        [oldView removeFromSuperview];
+                        model.snapshot = nil;
+                    }
+                }];
+            } else if (model.snapshot) {
+                [model.snapshot removeFromSuperview];
+                model.snapshot = nil;
             }
-            if (((AdModel*)_adModel).autocloseInterstitialTime > 0) {
-                [NSTimer scheduledTimerWithTimeInterval:((AdModel*)_adModel).autocloseInterstitialTime
-                                                 target:self 
-                                               selector:@selector(scheduledButtonAction) 
-                                               userInfo:nil 
-                                                repeats:NO];
+            
+            // Close button code
+            if (!self.closeButton && !self.adModel.useCustomClose) {
+                [self prepareResources];
+                
+                self.closeButton.frame = CGRectMake(self.frame.size.width - self.closeButton.frame.size.width - 11, 11, self.closeButton.frame.size.width, self.closeButton.frame.size.height);
+                self.closeButton.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin;
+                
+                self.closeButton.hidden = YES;
+                
+                [self addSubview:self.closeButton];
+                if (((AdModel*)_adModel).showCloseButtonTime > 0 && !((AdModel*)_adModel).isDisplayed) {
+                    [NSTimer scheduledTimerWithTimeInterval:((AdModel*)_adModel).showCloseButtonTime
+                                                     target:self 
+                                                   selector:@selector(showCloseButton)
+                                                   userInfo:nil 
+                                                    repeats:NO];
+                }
+                else {
+                    [self showCloseButton];
+                }
+                if (((AdModel*)_adModel).autocloseInterstitialTime > 0) {
+                    [NSTimer scheduledTimerWithTimeInterval:((AdModel*)_adModel).autocloseInterstitialTime
+                                                     target:self 
+                                                   selector:@selector(scheduledButtonAction) 
+                                                   userInfo:nil 
+                                                    repeats:NO];
+                }
             }
-		}
-        
-        if (!((AdModel*)_adModel).isDisplayed) {
-            ((AdModel*)_adModel).startDisplayDate = [NSDate date];
-            ((AdModel*)_adModel).isDisplayed = YES;
+            
+            if (!((AdModel*)_adModel).isDisplayed) {
+                ((AdModel*)_adModel).startDisplayDate = [NSDate date];
+                ((AdModel*)_adModel).isDisplayed = YES;
+            }
+            
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                [self postAdDisplaydNotification];
+            });
+            //[[NotificationCenter sharedInstance] postNotificationName:kAdDisplayedNotification object:self];
         }
 	}
 }
