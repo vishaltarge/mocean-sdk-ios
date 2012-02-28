2282acc7a6eb1899402793622151a298297319cf
Constantine Mureev <mureev@gmail.com>
2012-02-28 17:46:53 +0400

    update sample

--- Samples/AdMobileSamples/Classes/AdMobileSamplesViewController.h
+++ Samples/AdMobileSamples/Classes/AdMobileSamplesViewController.h
@@ -9,24 +9,17 @@
 
 // simple
 #import "SimpleBannerViewController.h"
-//#import "SimpleBannerRetinaViewController.h"
-//#import "InterstitialViewController.h"
-#import "VideoViewController.h"
 #import "OpenGLViewController.h"
 #import "RichJSadViewController.h"
 #import "InterstitialAdViewController.h"
 #import "ORMMAViewController.h"
 
-// animation
-#import "BannerAnimationViewController.h"
+// animation"
 #import "TableViewAnimationViewController.h"
 
 // advanced
 #import "TableViewCellSampleViewController.h"
-#import "VideoWithInterstitilaViewController.h"
-#import "RotationAdViewController.h"
 #import "DelegateViewController.h"
-#import "TestingViewController.h"
 
 @interface AdMobileSamplesViewController : UITableViewController {
     NSMutableArray*         _sections;
