1b6ac3e8d39cf2086ad59e883ba28510a32c3b85
Jason Dickert <jdickert@moceanmobile.com>
2012-04-13 02:07:00 -0400

    Set the library version to 2.11.0.

--- Sources/Classes/Shared/Core/Utils/MASTConstants.h
+++ Sources/Classes/Shared/Core/Utils/MASTConstants.h
@@ -6,7 +6,7 @@
  *
  */
 
-#define LIBRARY_VERSION             @"2.10.1"
+#define LIBRARY_VERSION             @"2.11.0"
 
 #define kDefaultAdServerUrl         @"http://ads.mocean.mobi/ad"
 #define kGoogleUrl                  @"http://www.google.com"
