260728b28c6a4f44a9d1a7c2322dfee88727818d
Jason Dickert <jdickert@moceanmobile.com>
2012-11-05 20:50:43 -0500

    Adding the InstallationDirect project sample that demonstrates how to include the MASTAd.Framework vs. the SDK Xcode project.  This allows for using pre-built MAST Ad Framework content (faster product builds, no need to build the SDK within a build) but doesn't provide inline debugging like the direct project method.

---
+++ Version3/Samples/InstallationFramework/InstallationFramework/MASTSViewController.h
@@ -0,0 +1,13 @@
+//
+//  MASTSViewController.h
+//  InstallationFramework
+//
+//  Created by Jason Dickert on 10/11/12.
+//  Copyright (c) 2012 Mocean Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MASTSViewController : UIViewController
+
+@end
